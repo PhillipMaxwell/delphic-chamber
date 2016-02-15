2b7e2888056bfb55929e574f10f3a50958dcd7f4
Phillip Maxwell <zenith_rising@yahoo.com>
2016-02-15 02:04:48 -0800

    update to styling and code

--- js/index.js
+++ js/index.js
@@ -1,3 +1,24 @@
+var template = function(text) {
+	return '<p>' + text + '</p>';
+};
+
+var add = function(item) {
+	var html=template(item);
+	$('div.comments-board').prepend(html);
+	
+
+}
+
+
 $(document).ready(function(){
 	$('#menu').accordion();
+
+	$('form').submit(function() {
+		var text=$('textarea#feedback').val();
+		add(text);
+		$('textarea#feedback').val(""); /* clear the comment box for the next comment */
+		return false;
+	});
+
+
 });
